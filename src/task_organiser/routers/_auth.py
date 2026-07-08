5691e76eb70184850cb9d59e80a2dd5c4b25831c
Vishal Pankaj Chandratreya <19171016+tfpf@users.noreply.github.com>
2026-07-09 00:27:49 +0530

    Token authentication check test

--- src/task_organiser/routers/_auth.py
+++ src/task_organiser/routers/_auth.py
@@ -1,11 +1,16 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from task_organiser import schemas, services
 
+oauth2_scheme = OAuth2PasswordBearer("/auth/signin")
 router = APIRouter(prefix="/auth")
+
+@router.get("/me", status_code=status.HTTP_200_OK, summary="View currently logged-in user")
+def me(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {}
 
 
 @router.post("/signin", status_code=status.HTTP_200_OK, summary="Log in")
